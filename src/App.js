dbd38fa07228f5de38f3bc1a1d6a594c37295c03
icortez <icortez@ufg.edu.sv>
2021-06-01 08:35:30 -0600

    building navbar

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import Home from './pages/Home';
 import Rooms from './pages/Rooms';
 import SingleRoom from './pages/SingleRoom';
 import Error from './pages/Error';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
+      <Navbar />
       <Switch>
         <Route path='/' exact component={Home} />
         <Route path='/rooms' exact component={Rooms} />
