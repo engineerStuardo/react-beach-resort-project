d90b6c870c4b9f27bdd7716bb31fd7035da95c00
icortez <icortez@ufg.edu.sv>
2021-06-01 11:15:59 -0600

    adding loading component and Room into featuredRooms

--- src/components/FeautreRooms.js
+++ src/components/FeautreRooms.js
@@ -1,12 +1,20 @@
 import React from 'react';
 
 import { useRoomContext } from '../context/customHook';
+import Loading from './Loading';
+import Room from './Room';
 
 const FeautreRooms = () => {
   const { featuredRooms: rooms } = useRoomContext();
   console.log(rooms);
 
-  return <div>hello from feautre rooms</div>;
+  return (
+    <div>
+      hello from feautre rooms
+      <Room />
+      <Loading />
+    </div>
+  );
 };
 
 export default FeautreRooms;
