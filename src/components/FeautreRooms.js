0a72b38f25274ba87d92df657008463283a8ebbb
icortez <icortez@ufg.edu.sv>
2021-06-01 11:41:37 -0600

    building featured-rooms

--- src/components/FeautreRooms.js
+++ src/components/FeautreRooms.js
@@ -3,17 +3,24 @@ import React from 'react';
 import { useRoomContext } from '../context/customHook';
 import Loading from './Loading';
 import Room from './Room';
+import Title from './Title';
 
 const FeautreRooms = () => {
-  const { featuredRooms: rooms } = useRoomContext();
-  console.log(rooms);
+  const { featuredRooms: rooms, loading } = useRoomContext();
+
+  if (loading) {
+    return <Loading />;
+  }
 
   return (
-    <div>
-      hello from feautre rooms
-      <Room />
-      <Loading />
-    </div>
+    <section className='featured-rooms'>
+      <Title title='featured rooms' />
+      <div className='featured-rooms-center'>
+        {rooms.map(room => (
+          <Room key={room.id} room={room} />
+        ))}
+      </div>
+    </section>
   );
 };
 
--- src/components/Room.js
+++ src/components/Room.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Room = () => {
-  return <div>hello from room</div>;
+import defaultImg from '../images/room-2.jpeg';
+
+const Room = ({ room: { name, slug, images, price } }) => {
+  return (
+    <article className='room'>
+      <div className='img-container'>
+        <img src={images[0] || defaultImg} alt='single room' />
+        <div className='price-top'>
+          <h6>{price}</h6>
+          <p>per night</p>
+        </div>
+        <Link to={`/rooms/${slug}`} className='btn-primary room-link'>
+          features
+        </Link>
+      </div>
+      <p className='room-info'>{name}</p>
+    </article>
+  );
 };
 
 export default Room;
