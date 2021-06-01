6a0d71ebfa314eda3ca166844a70f84f25a60a77
icortez <icortez@ufg.edu.sv>
2021-06-01 11:02:56 -0600

    setting up context and testing it in feature room

--- src/components/FeautreRooms.js
+++ src/components/FeautreRooms.js
@@ -3,7 +3,8 @@ import React from 'react';
 import { useRoomContext } from '../context/customHook';
 
 const FeautreRooms = () => {
-  const { greeting, name } = useRoomContext();
+  const { featuredRooms: rooms } = useRoomContext();
+  console.log(rooms);
 
   return <div>hello from feautre rooms</div>;
 };
--- src/context/context.js
+++ src/context/context.js
@@ -1,12 +1,30 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
+
+import items from '../data';
 
 export const RoomContext = createContext();
 
 export const RoomProvider = ({ children }) => {
   const [state, setState] = useState({
-    greeting: 'hello',
-    name: 'italo',
+    rooms: [],
+    sortedRooms: [],
+    featuredRooms: [],
+    loading: true,
   });
+
+  useEffect(() => {
+    const rooms = formatData(items);
+    const featuredRooms = rooms.filter(room => room.featured === true);
+    setState({ rooms, featuredRooms, sortedRooms: rooms, loading: false });
+  }, []);
+
+  const formatData = items => {
+    return items.map(item => {
+      const id = item.sys.id;
+      const images = item.fields.images.map(image => image.fields.file.url);
+      return { ...item.fields, images, id };
+    });
+  };
 
   return (
     <RoomContext.Provider value={{ ...state }}>{children}</RoomContext.Provider>
