612c56d6ccc296b67e6458bd1049d9f962274b32
icortez <icortez@ufg.edu.sv>
2021-06-01 09:41:50 -0600

    bulding banner and added to error,home and rooms componet

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import Hero from '../components/Hero';
+import Banner from '../components/Banner';
 
 const Home = () => {
-  return <Hero />;
+  return (
+    <Hero>
+      <Banner title='luxorious rooms' subtitle='deluxe rooms starting at$299'>
+        <Link to='/rooms' className='btn-primary'>
+          our rooms
+        </Link>
+      </Banner>
+    </Hero>
+  );
 };
 
 export default Home;
