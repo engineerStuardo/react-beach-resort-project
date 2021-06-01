01e0e16f66c1bfc8a9f8237b6d14550fac10246d
icortez <icortez@ufg.edu.sv>
2021-06-01 13:36:39 -0600

    finishing single room

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,8 +1,6 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Link, useParams } from 'react-router-dom';
 
-import defaultBcg from '../images/room-1.jpeg';
-import Hero from '../components/Hero';
 import Banner from '../components/Banner';
 import { StyledHero } from '../components/StyledHero';
 import { useRoomContext } from '../context/customHook';
@@ -36,13 +34,47 @@ const SingleRoom = () => {
   } = room;
 
   return (
-    <StyledHero img={images[0]}>
-      <Banner title={`${name} room`}>
-        <Link to='/rooms' className='btn-primary'>
-          Back to rooms
-        </Link>
-      </Banner>
-    </StyledHero>
+    <>
+      <StyledHero img={images[0]}>
+        <Banner title={`${name} room`}>
+          <Link to='/rooms' className='btn-primary'>
+            Back to rooms
+          </Link>
+        </Banner>
+      </StyledHero>
+      <section className='single-room'>
+        <div className='single-room-images'>
+          {images.map((image, index) => (
+            <img key={index} src={image} alt={name} />
+          ))}
+        </div>
+        <div className='single-room-info'>
+          <article className='desc'>
+            <h3>details</h3>
+            <p>{description}</p>
+          </article>
+          <article className='info'>
+            <h3>info</h3>
+            <h6>price : ${price}</h6>
+            <h6>size : ${size} SQFT</h6>
+            <h6>
+              max capacity :{' '}
+              {capacity > 1 ? `${capacity} people` : `${capacity} person`}
+            </h6>
+            <h6>{pets ? 'pets allowed' : 'no pets allowed'}</h6>
+            <h6>{breakfast && 'free breakfast included'}</h6>
+          </article>
+        </div>
+      </section>
+      <section className='room-extras'>
+        <h6>extras</h6>
+        <ul className='extras'>
+          {extras.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+      </section>
+    </>
   );
 };
 
