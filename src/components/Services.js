4f34fb5ca56e0e0cdb06501961b4417fc347c8cc
icortez <icortez@ufg.edu.sv>
2021-06-01 10:14:29 -0600

    building services and adding it to home component

---
+++ src/components/Services.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import { FaCocktail, FaHiking, FaShuttleVan, FaBeer } from 'react-icons/fa';
+
+import Title from './Title';
+
+const Services = () => {
+  const [services, setServices] = useState([
+    {
+      icon: <FaCocktail />,
+      title: 'Free Cocktails',
+      info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo aperiam, quas tempora obcaecati voluptas non.',
+    },
+    {
+      icon: <FaHiking />,
+      title: 'Endless Hiking',
+      info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo aperiam, quas tempora obcaecati voluptas non.',
+    },
+    {
+      icon: <FaShuttleVan />,
+      title: 'Strongest Beer',
+      info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo aperiam, quas tempora obcaecati voluptas non.',
+    },
+    {
+      icon: <FaBeer />,
+      title: 'Free cocktails',
+      info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo aperiam, quas tempora obcaecati voluptas non.',
+    },
+  ]);
+
+  return (
+    <section className='services'>
+      <Title title='services' />
+      <div className='services-center'>
+        {services.map((service, index) => {
+          return (
+            <article key={index} className='service'>
+              <span>{service.icon}</span>
+              <h6>{service.title}</h6>
+              <p>{service.info}</p>
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default Services;
